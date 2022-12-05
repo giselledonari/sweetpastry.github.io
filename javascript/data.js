231c83da4c16676aaedb69c5b57e9eb1e179ab50
giselledonari <giselle.donari@hotmail.com>
2022-12-05 12:01:27 -0300

    Actualizacion, se agregan funcionalidades javascript al carrito de compras

---
+++ javascript/data.js
@@ -0,0 +1,81 @@
+let productos=[{
+    id:1,
+    nombre:"Cheesecake Arandanos 1p",
+    precio:1990,
+},
+{
+    id:2,
+    nombre:"Cheesecake Arandanos 12p",
+    precio:12990,
+}
+,
+{
+    id:3,
+    nombre:"Torta de Chocolate 1p",
+    precio:1590,
+},
+{
+    id:4,
+    nombre:"Torta de Chocolate 16p",
+    precio:18990,
+},
+{
+    id:5,
+    nombre:"Torta Frutillas con Crema 1p",
+    precio:1590,
+},
+{
+    id:6,
+    nombre:"Torta Frutillas con Crema 16p",
+    precio:18990,
+},
+{
+    id:7,
+    nombre:"Donas de Chocolate 1u",
+    precio:1290,
+},
+{
+    id:8,
+    nombre:"Donas de Chocolate 4u",
+    precio:3990,
+},
+{
+    id:9,
+    nombre:"Profiteroles 1u",
+    precio:1490,
+},
+{
+    id:10,
+    nombre:"Profiteroles 4u",
+    precio:5490,
+},
+{
+    id:11,
+    nombre:"Queque de Manzana 12p",
+    precio:7990,
+},
+{
+    id:12,
+    nombre:"Pastel Tres Leches 1p",
+    precio:1790,
+},
+{
+    id:13,
+    nombre:"Pastel Tres Leches 16p",
+    precio:17990,
+},
+{
+    id:14,
+    nombre:"Queque de Limon 10p",
+    precio:6990,
+},
+{
+    id:15,
+    nombre:"Cheesecake Manjar 1p",
+    precio:1990,
+},
+{
+    id:16,
+    nombre:"Cheesecake Manjar 12p",
+    precio:19990,
+}];
